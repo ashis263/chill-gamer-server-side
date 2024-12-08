fc627ebcd2ca3aa3bd542ba21319d6597610afbe
Ashis Kumar Joy <ashis263@gmail.com>
2024-12-08 12:17:49 +0600

    added vercel.json and moved lines in index.js for deployment on vercel

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -33,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const reviews = client.db("gameReviewDB").collection('reviews');
     const users = client.db("gameReviewDB").collection('users');
@@ -124,8 +124,8 @@ async function run() {
     })
 
   // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  // await client.db("admin").command({ ping: 1 });
+  // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 } finally {
   // Ensures that the client will close when you finish/error
   // await client.close();
